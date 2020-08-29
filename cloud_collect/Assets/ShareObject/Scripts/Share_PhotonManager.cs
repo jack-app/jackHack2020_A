4315d3d763369067c0bf3063ee5db334f8a02f6d
DESKTOP-APK21IT\itoka229 <itoka229@yahoo.co.jp>
2020-08-29 21:32:15 +0900

    最低限Photonとつながるようにしておいた

---
+++ cloud_collect/Assets/ShareObject/Scripts/Share_PhotonManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Share_PhotonManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster");
+        // "room"という名前のルームに参加する（ルームが無ければ作成してから参加する）
+        PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions(), TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom");
+    }
+}
