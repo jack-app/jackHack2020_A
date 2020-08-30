a513aef1b58715b43b8942e20d490cc7c508994e
kumazawa_0210 <kumachan0220ios@gmail.com>
2020-08-30 21:46:36 +0900

    シーン遷移作成

---
+++ cloud_collect/Assets/Scripts/ClickFin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickFin : MonoBehaviour
+{
+    public CloudManager manger;
+    public GameObject rect;
+
+    public void OnClick()
+    {
+        manger.Stop();
+        rect.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
---
+++ cloud_collect/Assets/Scripts/Main2Fin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Main2Fin : MonoBehaviour
+{
+    public void OnClick()
+    {
+        SceneManager.LoadScene("Finish");
+    }
+}
