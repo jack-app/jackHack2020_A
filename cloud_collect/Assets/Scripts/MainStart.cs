6ac978e23851daab58ed2e087a90479da07882f8
kumazawa_0210 <kumachan0220ios@gmail.com>
2020-08-30 22:55:11 +0900

    ぬるっとシーン遷移

---
+++ cloud_collect/Assets/Scripts/MainStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainStart : MonoBehaviour
+{
+    public Image image;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DelayStart());
+    }
+
+    IEnumerator DelayStart()
+    {
+        for (int i = 0; i < 60; i++) {
+            image.color = new Color(1, 1, 1, i / 60.0f);
+            text.color = new Color(0, 0, 0, i / 60.0f);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
--- cloud_collect/Assets/Scripts/Title2Main.cs
+++ cloud_collect/Assets/Scripts/Title2Main.cs
@@ -2,11 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Title2Main : MonoBehaviour
 {
+    public List<Text> texts;
+    public List<SpriteRenderer> images;
+
     public void OnClick()
     {
+        StartCoroutine(DelayNextScene());
+    }
+
+    IEnumerator DelayNextScene()
+    {
+        float h = 193;
+        float s_end = 82;
+        float v_end = 90;
+
+        var statrt = Time.time;
+
+        for (int i = 0; i < 60; i++) {
+            var s = Mathf.Lerp(0, s_end, i / 60.0f);
+            var v = Mathf.Lerp(100, v_end, i / 60.0f);
+            texts.ForEach(e => e.color = Color.HSVToRGB(h / 360.0f, s / 100.0f, v / 100.0f));
+            images.ForEach(e => e.color = Color.HSVToRGB(h / 360.0f, s / 100.0f, v / 100.0f));
+            yield return new WaitForEndOfFrame();
+        }
+
         SceneManager.LoadScene("Main");
     }
 }
