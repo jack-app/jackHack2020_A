19178954a472bb5b882d2a786c7a6d2993a330ba
kumazawa_0210 <kumachan0220ios@gmail.com>
2020-08-31 14:32:57 +0900

    fix4

--- cloud_collect/Assets/Plugins/src.jslib
+++ cloud_collect/Assets/Plugins/src.jslib
@@ -4,4 +4,9 @@ mergeInto(LibraryManager.library, {
     console.log('called')
     window.open(Pointer_stringify(_url),'_blank')
   },
+
+  TakeScreenShot : function () {
+    console.log('called')
+    var png = document.getElementById('canvas').toDataURL('screenShot/png');
+}
 });
--- cloud_collect/Assets/TakePicture/PictureBehaviour.cs
+++ cloud_collect/Assets/TakePicture/PictureBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEditor;
@@ -9,13 +10,15 @@ using UnityEngine;
 public class PictureBehaviour : MonoBehaviour
 {
     private string _filename = "screenShot";
+    [DllImport("__Internal")]
+    private static extern void TakeScreenShot();
     /// <summary>
     /// 画像を撮影して指定のファイルパスに保存、ファイルパスを戻り値として渡す。
     /// </summary>
     public string TakePicture()
     {
         string filepath = Application.dataPath + "/" + _filename + ".png";
-        ScreenCapture.CaptureScreenshot(filepath);
+        Screenshot(filepath);
         CancellationTokenSource source = new CancellationTokenSource();
         CancellationToken cancellationToken = source.Token;
 
@@ -26,6 +29,21 @@ public class PictureBehaviour : MonoBehaviour
 
 
         return filepath;
+    }
+
+    private void Screenshot(string path)
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        TakeScreenShot();
+#else
+        ScreenCapture.CaptureScreenshot(path);
+#endif
+
+        var currentScreenShotTexture = new Texture2D(Screen.width, Screen.height);
+        currentScreenShotTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        currentScreenShotTexture.Apply();
+
+        File.WriteAllBytes(path, currentScreenShotTexture.EncodeToPNG());
     }
 
     async void Timeout(CancellationTokenSource source) {
